ce08a2b806b1d2a40e317e040bfaca7918571c30
festa78 <nonamehorses78@gmail.com>
2018-02-21 06:59:59 +0000

    traning U-Net with data augmentation

---
+++ main.py
@@ -0,0 +1,174 @@
+#!/usr/bin/python3 -B
+
+import os
+import random
+import sys
+import warnings
+
+from itertools import chain
+from keras.models import Model, load_model
+from keras.layers import Input
+from keras.layers.core import Dropout, Lambda
+from keras.layers.convolutional import Conv2D, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.merge import concatenate
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras import backend as K
+import numpy as np
+from PIL import Image
+from PIL import ImageEnhance
+from skimage.io import imread, imshow, imread_collection, concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+import tensorflow as tf
+from tqdm import tqdm
+
+from preprocess import central_scale_images, rotate_images, flip_images
+
+# Set some parameters
+IMG_WIDTH = 128
+IMG_HEIGHT = 128
+IMG_CHANNELS = 3
+TRAIN_PATH = '../input/stage1_train/'
+TEST_PATH = '../input/stage1_test/'
+
+warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
+seed = 42
+random.seed = seed
+np.random.seed = seed
+
+# # Get train and test IDs
+# train_ids = next(os.walk(TRAIN_PATH))[1]
+# test_ids = next(os.walk(TEST_PATH))[1]
+
+# # Get and resize train images and masks
+# X_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+# Y_train = np.zeros((len(train_ids), IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+# print('Getting and resizing train images and masks ... ')
+# sys.stdout.flush()
+# for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
+#     path = TRAIN_PATH + id_
+#     img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+#     img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+#     X_train[n] = img
+#     mask = np.zeros((IMG_HEIGHT, IMG_WIDTH, 1), dtype=np.bool)
+#     for mask_file in next(os.walk(path + '/masks/'))[2]:
+#         mask_ = imread(path + '/masks/' + mask_file)
+#         mask_ = np.expand_dims(resize(mask_, (IMG_HEIGHT, IMG_WIDTH), mode='constant',
+#                                       preserve_range=True), axis=-1)
+#         mask = np.maximum(mask, mask_)
+#     Y_train[n] = mask
+
+# # Get and resize test images
+# X_test = np.zeros((len(test_ids), IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS), dtype=np.uint8)
+# sizes_test = []
+# print('Getting and resizing test images ... ')
+# sys.stdout.flush()
+# for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
+#     path = TEST_PATH + id_
+#     img = imread(path + '/images/' + id_ + '.png')[:,:,:IMG_CHANNELS]
+#     sizes_test.append([img.shape[0], img.shape[1]])
+#     img = resize(img, (IMG_HEIGHT, IMG_WIDTH), mode='constant', preserve_range=True)
+#     X_test[n] = img
+
+# print('Done!')
+
+# # Save images for convenience.
+# np.save('X_train.npy', X_train)
+# np.save('Y_train.npy', Y_train)
+# np.save('X_test.npy', X_test)
+
+# Load images.
+X_train = np.load('X_train.npy')
+Y_train = np.load('Y_train.npy')
+X_test = np.load('X_test.npy')
+
+# Data augmentation.
+print('data augmentation')
+print('scale images')
+X_train_scale, Y_train_scale = central_scale_images(X_train, Y_train, [1., 0.9, 0.8, 0.7])
+
+print('rotate images')
+X_train_rot, Y_train_rot = rotate_images(X_train, Y_train)
+
+print('flip images')
+X_train_flip, Y_train_flip = flip_images(X_train, Y_train)
+
+X_train = np.concatenate((X_train, X_train_scale, X_train_rot, X_train_flip))
+Y_train = np.concatenate((Y_train, Y_train_scale, Y_train_rot, Y_train_flip))
+
+# Define IoU metric
+def mean_iou(y_true, y_pred):
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        y_pred_ = tf.to_int32(y_pred > t)
+        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([up_opt]):
+            score = tf.identity(score)
+        prec.append(score)
+    return K.mean(K.stack(prec), axis=0)
+
+# U-net construction.
+inputs = Input((IMG_HEIGHT, IMG_WIDTH, IMG_CHANNELS))
+s = Lambda(lambda x: x / 255) (inputs)
+
+c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (s)
+c1 = Dropout(0.1) (c1)
+c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c1)
+p1 = MaxPooling2D((2, 2)) (c1)
+
+c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p1)
+c2 = Dropout(0.1) (c2)
+c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c2)
+p2 = MaxPooling2D((2, 2)) (c2)
+
+c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p2)
+c3 = Dropout(0.2) (c3)
+c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c3)
+p3 = MaxPooling2D((2, 2)) (c3)
+
+c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p3)
+c4 = Dropout(0.2) (c4)
+c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c4)
+p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+
+c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p4)
+c5 = Dropout(0.3) (c5)
+c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c5)
+
+u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same') (c5)
+u6 = concatenate([u6, c4])
+c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u6)
+c6 = Dropout(0.2) (c6)
+c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c6)
+
+u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
+u7 = concatenate([u7, c3])
+c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u7)
+c7 = Dropout(0.2) (c7)
+c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c7)
+
+u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c7)
+u8 = concatenate([u8, c2])
+c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u8)
+c8 = Dropout(0.1) (c8)
+c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c8)
+
+u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c8)
+u9 = concatenate([u9, c1], axis=3)
+c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
+c9 = Dropout(0.1) (c9)
+c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c9)
+
+outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
+
+model = Model(inputs=[inputs], outputs=[outputs])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[mean_iou])
+model.summary()
+
+# Fit model
+earlystopper = EarlyStopping(patience=5, verbose=1)
+checkpointer = ModelCheckpoint('model-dsbowl2018-1.h5', verbose=1, save_best_only=True)
+results = model.fit(X_train, Y_train, validation_split=0.1, batch_size=16, epochs=50,
+                    callbacks=[earlystopper, checkpointer])
---
+++ preprocess.py
@@ -0,0 +1,97 @@
+# Ref:
+#   - https://medium.com/ymedialabs-innovation/data-augmentation-techniques-in-cnn-using-tensorflow-371ae43d5be9
+#   - https://www.kaggle.com/keegil/keras-u-net-starter-lb-0-277/notebook
+
+import tensorflow as tf
+import matplotlib.image as mpimg
+import numpy as np
+
+
+def central_scale_images(X_imgs, Y_imgs, scales):
+    n ,h, w, c = X_imgs.shape
+
+    # Various settings needed for Tensorflow operation
+    boxes = np.zeros((len(scales), 4), dtype = np.float32)
+    for index, scale in enumerate(scales):
+        x1 = y1 = 0.5 - 0.5 * scale # To scale centrally
+        x2 = y2 = 0.5 + 0.5 * scale
+        boxes[index] = np.array([y1, x1, y2, x2], dtype = np.float32)
+    box_ind = np.zeros((len(scales)), dtype = np.int32)
+    crop_size = np.array([h, w], dtype = np.int32)
+
+    X_scale_data = []
+    Y_scale_data = []
+    tf.reset_default_graph()
+    X = tf.placeholder(tf.float32, shape = (1, h, w, 3))
+    Y = tf.placeholder(tf.float32, shape = (1, h, w, 1))
+    # Define Tensorflow operation for all scales but only one base image at a time
+    tf_X_img = tf.image.crop_and_resize(X, boxes, box_ind, crop_size)
+    tf_Y_img = tf.image.crop_and_resize(Y, boxes, box_ind, crop_size)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for img_data, mask_data in zip(X_imgs, Y_imgs):
+            batch_img = np.expand_dims(img_data, axis = 0)
+            batch_mask = np.expand_dims(mask_data, axis = 0)
+            scaled_imgs = sess.run(tf_X_img, feed_dict = {X: batch_img})
+            scaled_masks = sess.run(tf_Y_img, feed_dict = {Y: batch_mask})
+            X_scale_data.extend(scaled_imgs)
+            Y_scale_data.extend(scaled_masks)
+
+    X_scale_data = np.array(X_scale_data, dtype = np.float32)
+    Y_scale_data = np.array(Y_scale_data, dtype = np.float32)
+    return X_scale_data, Y_scale_data
+
+
+def rotate_images(X_imgs, Y_imgs):
+    n ,h, w, c = X_imgs.shape
+
+    X_rotate = []
+    Y_rotate = []
+    tf.reset_default_graph()
+    X = tf.placeholder(tf.float32, shape = (h, w, 3))
+    Y = tf.placeholder(tf.float32, shape = (h, w, 1))
+    k = tf.placeholder(tf.int32)
+    tf_img = tf.image.rot90(X, k = k)
+    tf_mask = tf.image.rot90(Y, k = k)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for img, mask in zip(X_imgs, Y_imgs):
+            i = np.random.randint(3)  # Rotation at 90, 180, or 270.
+            rotated_img = sess.run(tf_img, feed_dict = {X: img, k: i + 1})
+            rotated_mask = sess.run(tf_mask, feed_dict = {Y: mask, k: i + 1})
+            X_rotate.append(rotated_img)
+            Y_rotate.append(rotated_mask)
+
+    X_rotate = np.array(X_rotate, dtype = np.float32)
+    Y_rotate = np.array(Y_rotate, dtype = np.float32)
+    return X_rotate, Y_rotate
+
+
+def flip_images(X_imgs, Y_imgs):
+    n ,h, w, c = X_imgs.shape
+
+    X_flip = []
+    Y_flip = []
+    tf.reset_default_graph()
+    X = tf.placeholder(tf.float32, shape = (h, w, 3))
+    Y = tf.placeholder(tf.float32, shape = (h, w, 1))
+    tf_imgX1 = tf.image.flip_left_right(X)
+    tf_imgX2 = tf.image.flip_up_down(X)
+    tf_imgX3 = tf.image.transpose_image(X)
+    tf_imgY1 = tf.image.flip_left_right(Y)
+    tf_imgY2 = tf.image.flip_up_down(Y)
+    tf_imgY3 = tf.image.transpose_image(Y)
+    tf_imgX = [tf_imgX1, tf_imgX2, tf_imgX3]
+    tf_imgY = [tf_imgY1, tf_imgY2, tf_imgY3]
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for img, mask in zip(X_imgs, Y_imgs):
+            i = np.random.randint(3)
+            flipped_img = sess.run(tf_imgX[i], feed_dict = {X: img})
+            flipped_mask = sess.run(tf_imgY[i], feed_dict = {Y: mask})
+            X_flip.append(flipped_img)
+            Y_flip.append(flipped_mask)
+    X_flip = np.array(X_flip, dtype = np.float32)
+    Y_flip = np.array(Y_flip, dtype = np.float32)
+    return X_flip , Y_flip
